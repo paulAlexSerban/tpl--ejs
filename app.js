7e003cf35995ce741dc0b2216e20f6ae44b0167f
paulAlexSerban <paul.alex.serban@gmail.com>
2021-12-13 00:17:27 +0200

    ejs_04 | implemented ejs

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const rootDir = require('./util/path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-const app = express();
-
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// give the user access to the `./public` directory
-app.use(express.static(path.join(rootDir, 'public')));
-
-app.use('/admin',adminData.routes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
-
